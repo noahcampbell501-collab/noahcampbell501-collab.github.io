f5cd1bb150a2af34f29192a59e3cad725193292a
noahcampbell501-collab <noahcampbell501@gmail.com>
2026-04-28 16:07:37 -0500

    Update final.js

--- final/js/final.js
+++ final/js/final.js
@@ -5,16 +5,16 @@ function handleAskName() {
 
   let message = '';
 
-  if (askName === 0) {
-    message = 'Go ahead, I prepared for everything!';
-  } else if (askName === 1) {
+   if (askName === 1) {
     message = 'Thrindel Yorgenfist.';
   } else if (askName === 2) {
     message = '...er, Tarben. Tarben the short.';
   } else if (askName === 3) {
+    message = 'Tarben... The tall?';
+  } else if (askName === 4) {
     message = 'tt-Terry? I guess?';
   } else {
-    message = 'This is Terry ' + (askName - 1) + ', alright? Now leave me alone!';
+    message = 'This is Terry ' + (askName -3) + ', alright? Now leave me alone!';
   }
   
   askName++; // increment the counter
